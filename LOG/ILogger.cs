485cbda2db27d119018c08e3d2ed3fdaf9f688d0
OksanaPalianytsia <oksana.palianytsia@lnu.edu.ua>
2021-11-22 23:18:30 +0200

    Logging added

---
+++ LOG/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace LOG
+{
+    public interface ILogger
+    {
+        void Info(string message);
+        void Debug(string message);
+    }
+}
---
+++ LOG/Logger.cs
@@ -0,0 +1,29 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOG
+{
+    public class Logger : ILogger
+    {
+        private  log4net.ILog _log;
+
+        public Logger(Type t)
+        {
+            _log = LogManager.GetLogger(t);
+            log4net.Config.XmlConfigurator.Configure();
+        }
+        public void Debug(string message)
+        {
+           _log.Debug(message);
+        }
+
+        public void Info(string message)
+        {
+            _log.Info(message);
+        }
+    }
+}
